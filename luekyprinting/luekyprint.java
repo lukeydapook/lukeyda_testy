96e1c6a25d367810e808f4d49d361a83dc84e079
lueky_pueky <142909713+lukeydapook@users.noreply.github.com>
2023-08-29 13:59:09 +0000

    my first program submitted by me

---
+++ luekyprinting/luekyprint.java
@@ -0,0 +1,8 @@
+class luekytest
+{
+    public static void main(String args[])
+    {
+        System.out.println("welcome bobo");
+        System.out.print("user name: bobo boberson");
+    }
+}
---
+++ luekytest/luekytest.java
@@ -0,0 +1,8 @@
+class luekytest
+{
+    public static void main(String args[])
+    {
+        System.out.println("meiw meaw");
+        System.out.print("(hello world)");
+    }
+}
