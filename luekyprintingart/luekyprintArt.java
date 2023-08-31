2b7aff6e096d20611ccb4a692525835e621c60a3
lueky_pueky <142909713+lukeydapook@users.noreply.github.com>
2023-08-31 13:55:28 +0000

    made art program

---
+++ luekyprintingart/luekyprintArt.java
@@ -0,0 +1,25 @@
+class luekyprintingArt
+{
+    public static void main(String args[])
+    {
+        System.out.println("hi");
+
+        System.out.println("");
+
+        System.out.println(" ______________");
+        System.out.println("|     ~    _   |");
+        System.out.println("|     |    |   |");
+        System.out.println("|  |__________||");
+        System.out.println("|______________|");
+        System.out.println("   |  |    |  |");
+        System.out.println("   |  |    |  |");
+        System.out.println("   |  |    |  |");
+        System.out.println("   |  |    |  |");
+        System.out.println("   |  |__  |  |__");
+        System.out.println("   |_____| |_____|");
+
+        System.out.println("");
+
+        System.out.println("tanks!1!");
+    }
+}
