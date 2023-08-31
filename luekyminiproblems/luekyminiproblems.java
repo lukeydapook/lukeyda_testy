650ca7d7a285e1e2b3a0c2f25e199942742fa249
lueky_pueky <142909713+lukeydapook@users.noreply.github.com>
2023-08-31 21:39:00 +0000

    made miniproblems

---
+++ luekyminiproblems/luekyminiproblems.java
@@ -0,0 +1,30 @@
+class luekyminiproblems
+{
+    public static void main(String args[])
+    {
+        System.out.println("bello i have pattern 4 u!");
+
+        System.out.println("");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        System.out.println("_______");
+        System.out.println("next up");
+        System.out.println("_______");
+        /*next up*/
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
