8a1d854e489a77ed582f9eea7c6718a31abf052d
lueky_pueky <142909713+lukeydapook@users.noreply.github.com>
2023-08-31 13:33:28 +0000

    made printer pattern

---
+++ luekyextraprinting/NumberPrinterPattern.java
@@ -0,0 +1,19 @@
+/*luke herndon's numbers*/
+class NumberPrinterPattern
+{
+    public static void main(String args[])
+    {
+        System.out.println("welcom!!!1!!11");
+
+        System.out.println("");
+
+        System.out.println("1"); 
+        System.out.println("2 3");
+        System.out.println("4 5 6");
+        System.out.println("7 8 9 10");
+
+        System.out.println("");
+
+        System.out.print("thank u biey!!!1!1!!11");
+    }
+}
--- luekyprinting/luekyprint.java
+++ luekyprinting/luekyprint.java
@@ -1,8 +1,21 @@
-class luekytest
+class luekyprint
 {
     public static void main(String args[])
     {
-        System.out.println("welcome bobo");
-        System.out.print("user name: bobo boberson");
+        System.out.println("welcome user");
+
+        System.out.println("");
+
+        System.out.print("user name: lueky pookie");
+
+        System.out.println("");
+        System.out.println("");
+
+        System.out.print("user age: 12");
+
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("thanks monkey, bye");
     }
 }
