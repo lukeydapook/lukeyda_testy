ed7955378af059f02e1612c879cb2b1a665c8f58
lueky_pueky <142909713+lukeydapook@users.noreply.github.com>
2023-08-31 14:13:12 +0000

    made short storie

---
+++ luekyshortstory/luekyshortstory.java
@@ -0,0 +1,27 @@
+class luekyshortstory
+{
+    public static void main(String args[])
+    {
+        System.out.println("one apon tiem tere was kat");
+
+        System.out.println("");
+
+        System.out.println("kat said to cat,");
+        
+        System.out.println("");
+
+        System.out.println("meow mewo which means hello cat");
+        System.out.println("what happen to lil cat? said cat");
+        System.out.println("he went to boogie said kat");
+
+        System.out.println("");
+
+        System.out.println("lil cat was actually not boogieing, he was in fact doing a lil dance");
+        System.out.println("kat went to go find lil cat and started hiting a joggie with lil cat");
+
+        System.out.println("");
+
+        System.out.println("cat joined in and they boogied all day long");
+
+    }
+}
