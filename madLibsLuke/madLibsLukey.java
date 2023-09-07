1aa91f53ea27e9b2343f058d2d49ea21596a0e81
lueky_pueky <142909713+lukeydapook@users.noreply.github.com>
2023-09-07 13:48:33 +0000

    made angry libs

---
+++ madLibsLuke/madLibsLukey.java
@@ -0,0 +1,37 @@
+class madLibsLukey
+{
+    public static void main(String args[])
+    {
+        String name1 = "[name1]";
+
+        String name2 = "[name2]";
+
+        String isle1 = "[isle1]";
+
+        String isle2 = "[isle2]";
+
+        String item1 = "[item1]";
+
+        String item2 = "[item2]";
+
+        String dessert = "[dessert]";
+
+        String peanutButterType = "[peanutButterType]";
+
+        String typeOfNeighborhood = "[typeOfNeighborhood]";
+
+        String tvShow = "[tvShow]";
+
+        System.out.println("welcome to madlibs");
+
+        System.out.println("");
+
+        System.out.println("one day " + name1 +" went to the store for some blueberries.");
+        System.out.println("while at the store, " + name1 + " saw " + name2 + " at the " + isle1 + " isle.");
+        System.out.println(name1 + " and " + name2 + " walked to the " + isle2 + " to get some " + item1 + ".");
+        System.out.println("instead of getting blueberries " + name1 + " got some " + peanutButterType + " and " + item2);
+        System.out.println(name1 + " and " + name2 + " went to " + name1 + "'s house in the " + typeOfNeighborhood + ".");
+        System.out.println("they both had " + dessert + " and watched " + tvShow + ".");
+
+    }
+}
